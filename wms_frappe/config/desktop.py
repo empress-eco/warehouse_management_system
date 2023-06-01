9e437703a26054e7f872a79b96f42ded3a953ceb
Jeans K. Real <jkrb7@hotmail.com>
2023-06-01 07:30:55 -0600

    feat: Initialize App

---
+++ wms_frappe/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Warehouse Management System",
+			"type": "module",
+			"label": _("Warehouse Management System")
+		}
+	]
